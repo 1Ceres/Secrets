78d6a3de497f9474bc0df67becfaf2d411ec4f79
alkoWebDeveloper <alkowebdeveloper@gmail.com>
2022-06-14 21:56:02 +0100

    added some functionality in top of authorization

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", () => console.log("MongoDB 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -88,13 +89,26 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.get("/secrets", function(req, res){
+app.get("/submit", function(req, res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }
   else{
     res.redirect("/login");
   }
+});
+
+app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers})
+      }
+    }
+  });
 });
 
 app.get("/logout", function(req, res){
@@ -138,10 +152,26 @@ app.post("/login", function(req, res) {
       });
     }
   });
-
 });
 
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
 
+User.findById(req.user.id, function(err, foundUser){
+  if(err){
+    console.log(err);
+  }
+  else{
+    if (foundUser){
+      foundUser.secret = submittedSecret;
+      foundUser.save(function(){
+        res.redirect("/secrets");
+      });
+    }
+  }
+});
+
+});
 
 
 
